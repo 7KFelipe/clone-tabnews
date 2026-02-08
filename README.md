550bd3e1d43f79eccbb9a509b83e1f007d8fd9c3
Felipe Vasconcelos <felipe96018629@gmail.com>
2026-02-08 15:23:50 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Experimento do curso para aprender a trabalhar com js e aplicações com maior volume de usuários
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mamão para de ver vídeo no triturador de mentes</h1>
+  return <h1>Mamão para de ver vídeo no triturador de mentes</h1>;
 }
 
 export default Home;
