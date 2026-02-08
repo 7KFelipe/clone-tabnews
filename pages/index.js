1e00e354b0b76992ee6d717c086bfe713b6f54a6
Felipe Vasconcelos <felipe96018629@gmail.com>
2026-02-08 15:07:57 -0300

    versão Lucas

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Mãe eu te amo, se você me ama faz um pix para mim de R$1.000</h1>
+    return <h1>Mamão para de ver vídeo no triturador de mentes</h1>
 }
 
 export default Home;
